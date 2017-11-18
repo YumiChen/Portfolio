726f90210fb80dc1633a033c2d387d5000ef5d10
101506002 <amy830221@gmail.com>
2017-11-18 15:48:06 +0900

    scroll fixed

--- src/functions/scroll.js
+++ src/functions/scroll.js
@@ -2,12 +2,10 @@ var $ = require('jquery/src/core');
 require('jquery/src/selector');
 require('jquery/src/effects');
 require('jquery/src/css');
+require('jquery/src/offset');
 
 function scroll(dom){
-    const offset = document.querySelector(dom).getBoundingClientRect().top + document.body.scrollTop;
-
-    $('html,body').animate({scrollTop: offset}, 500);
-
+    $('body').animate({scrollTop: $('body').scrollTop() + $(dom).offset().top}, 500);
 };
 
 module.exports = scroll;
