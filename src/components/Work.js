596d67527f51923e9b43f8c650da15084a70c787
101506002 <amy830221@gmail.com>
2017-12-25 18:58:20 +0900

    work(reservation site) added

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,11 +8,11 @@ const Work = (props)=>{
      });
     
      return (<div className="work">
-        <a target="blank" href={props.demoUrls}>
+        <a target="blank" href={props.demoUrls || props.githubUrls}>
            <div className="img"/>
         </a>
          <p className="demoUrls">{props.demoUrls===""?null:<a href={props.demoUrls} target="_blank">Demo</a> } 
-    {props.githubUrls===""?null:" |" }        {props.githubUrls===""?null:<a href={props.githubUrls} target="_blank">Github Source</a>}</p>
+    {props.githubUrls && props.demoUrls?" |":null }        {props.githubUrls===""?null:<a href={props.githubUrls} target="_blank">Github Source</a>}</p>
            <p>{props.content.title}</p>
            <p className="context">{props.content.description}</p>
            <p>{tags}</p>
