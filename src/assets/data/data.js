cbde6762cc47541f21aa0f28a798fc5fc96f276d
Yumi Chen <monorhapsody@gmail.com>
2019-04-30 21:52:37 +0800

    update illustration website url public/bundle.js src/assets/data/data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -11,7 +11,7 @@ CH:"我是Yumi, 現為任職於科技公司的前端工程師，具開發大型�
     "https://play.google.com/store/apps/details?id=com.stevia_network.watpo",
     "https://yumichen.github.io/Rhythm-Game/index.html",
     "https://yumichen.github.io/CMS-UI-Generator/public/index.html#" ,  
-    "https://yumichen.github.io/Website/public/index.html",
+    "https://yumichen.github.io/Website/",
     // "https://chatchatiscool.herokuapp.com/",
     "",
     "https://yumichen.github.io/BookShelf/public/index.html#/"
