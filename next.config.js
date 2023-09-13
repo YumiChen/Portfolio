b82ecc5cc1af5ad1b13e86c9a9abf460f37c6c9d
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-09-12 22:27:04 -0400

    Add assetPrefix config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   output: 'export',
   // workaround for site to be accessible from /portfolio/public route
   basePath: isProd ? '/portfolio/public' : undefined,
+  assetPrefix: isProd ? 'https://cdn.jsdelivr.net/gh/YumiChen/portfolio@latest/dist/public' : undefined,
   distDir: 'dist/public'
 }
 
