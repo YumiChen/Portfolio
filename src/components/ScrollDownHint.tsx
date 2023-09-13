77a858a9071b7768fabf680d106156d087cde4b8
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-09-13 15:31:36 -0400

    Fix scroll down hint styles

--- src/components/ScrollDownHint.tsx
+++ src/components/ScrollDownHint.tsx
@@ -2,7 +2,7 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ScrollDownHint = ({ hintClass = ''})=>{
-    return (<div className={`flex flex-col py-5 pb-7 w-screen text-center ${hintClass}`}>
+    return (<div className={`flex flex-col py-5 pb-7 w-full text-center ${hintClass}`}>
     <span className="text-base md:text-lg">SCROLL DOWN</span>
     <FontAwesomeIcon icon={faChevronDown} className="text-3xl nd:text-5xl float"/>
 </div>);
