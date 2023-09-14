ab9d1b7a88939345e5ed2a6910aae7657e3ee1a9
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-09-14 17:06:29 -0400

    Fix swiper not initialized successfully in Experiences section

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -5,8 +5,39 @@ import LINE003Image from '../../public/line003.jpg';
 import MB001Image from '../../public/mb001.jpg';
 import MB002Image from '../../public/mb002.jpg';
 import MB003Image from '../../public/mb003.jpg';
+import { useEffect } from 'react';
+import { SwiperElement } from '@/app/types';
 
 const Experiences = () =>{
+  useEffect(()=>{
+    setTimeout(()=>{
+        const lineSwiper: SwiperElement = document.querySelector('#line-swiper')
+        if(!lineSwiper){
+          return;
+        }
+
+        Object.assign(lineSwiper, {
+          pagination: true,
+          autoplay: true,
+          loop: true,
+          spaceBetween: 10
+        });
+        lineSwiper.initialize();
+
+        const musicbravoSwiper: SwiperElement = document.querySelector('#musicbravo-swiper')
+        if(!musicbravoSwiper){
+          return;
+        }
+        Object.assign(musicbravoSwiper, {
+          pagination: true,
+          autoplay: true,
+          loop: true,
+          spaceBetween: 10
+        });
+        musicbravoSwiper.initialize();
+    }, 0);
+  }, []);
+
     return (<section className='md:min-h-[85vh] overflow-x-hidden overflow-y-auto px-10 md:px-40 pb-10'>
         <div className="border-l-[10px] pl-4 border-amber-300">
           <h3 className="font-serif font-bold mt-10 text-4xl md:text-5xl md:mb-3">LINE Corp.</h3>
@@ -18,15 +49,15 @@ const Experiences = () =>{
           </p>
         </div>
         <div className="flex flex-col lg:flex-row-reverse items-start">
-          <swiper-container pagination={true} autoplay={true} class="w-full lg:w-1/2" loop={true} space-between="10">
+          <swiper-container id="line-swiper" class="w-full lg:w-1/2" init={false}>
             <swiper-slide>
-              <Image src={LINE001Image} alt="LINE Today" width={500} height={400} className="my-10 mx-auto lg:ml-auto lg:mr-0" />
+              <Image src={LINE001Image} alt="LINE Today" width={500} height={378} className="my-10 mx-auto lg:ml-auto lg:mr-0" />
             </swiper-slide>
             <swiper-slide>
-              <Image src={LINE002Image} alt="LINE article" width={500} height={400} className="my-10 mx-auto lg:ml-auto lg:mr-0" />
+              <Image src={LINE002Image} alt="LINE article" width={500} height={378} className="my-10 mx-auto lg:ml-auto lg:mr-0" />
             </swiper-slide>
             <swiper-slide>
-              <Image src={LINE003Image} alt="LINE event" width={500} height={400} className="my-10 mx-auto lg:ml-auto lg:mr-0" />
+              <Image src={LINE003Image} alt="LINE event" width={500} height={378} className="my-10 mx-auto lg:ml-auto lg:mr-0" />
             </swiper-slide>    
           </swiper-container>
           <ul className="text-2xl md:text-3xl lg:pr-1">
@@ -80,15 +111,15 @@ const Experiences = () =>{
           </p>
         </div>
         <div className="flex flex-col lg:flex-row-reverse items-start">          
-          <swiper-container pagination={true} autoplay={true} class="w-full lg:w-1/2" loop={true} space-between="10">
+          <swiper-container id="musicbravo-swiper" class="w-full lg:w-1/2" init={false}>
             <swiper-slide>
-              <Image src={MB001Image} width={500} height={400} alt="Music Bravo home page" className="my-10 mx-auto lg:ml-auto lg:mr-0" />
+              <Image src={MB001Image} width={500} height={293} alt="Music Bravo home page" className="my-10 mx-auto lg:ml-auto lg:mr-0" />
             </swiper-slide>
             <swiper-slide>
-              <Image src={MB002Image} width={500} height={400} alt="Payment integration" className="my-10 mx-auto lg:ml-auto lg:mr-0" />
+              <Image src={MB002Image} width={500} height={293} alt="Payment integration" className="my-10 mx-auto lg:ml-auto lg:mr-0" />
             </swiper-slide>
             <swiper-slide>
-              <Image src={MB003Image} width={500} height={400} alt="Campaign page" className="my-10 mx-auto lg:ml-auto lg:mr-0" />
+              <Image src={MB003Image} width={500} height={293} alt="Campaign page" className="my-10 mx-auto lg:ml-auto lg:mr-0" />
             </swiper-slide>    
           </swiper-container>
           <ul className="text-2xl md:text-3xl lg:pr-1">
