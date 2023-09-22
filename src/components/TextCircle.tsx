abc5d2d961e5ddc37f6e571a6b9eedc8452b263b
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-09-22 02:06:09 -0400

    Add key in interator in TextCircle

--- src/components/TextCircle.tsx
+++ src/components/TextCircle.tsx
@@ -6,7 +6,7 @@ const TextCircle = ({ texts }: { texts: string }) =>{
     }
     return (<div className='ratate-text relative w-0 origin-center transform'>
             {textArray.map((text, index)=>(
-                <span className={`text-lg h-[190px] md:text-4xl md:h-[330px] lg:h-[430px] text-yellow-200 absolute bottom-0 origin-[bottom_center] transform hover:bg-transparent`} style={{
+                <span key={`${text}-${index}`} className={`text-lg h-[190px] md:text-4xl md:h-[330px] lg:h-[430px] text-yellow-200 absolute bottom-0 origin-[bottom_center] transform hover:bg-transparent`} style={{
                     transform: `translateX(-50%) rotate(${Math.ceil(index * 360 / textArray.length)}deg)`
                 }}>
                     {text}
