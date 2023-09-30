e9119f5f15661eb0082a32ef56da74579e9e4398
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-09-30 01:11:45 -0400

    Modify Top section styles

--- src/components/ScrollDownHint.tsx
+++ src/components/ScrollDownHint.tsx
@@ -2,8 +2,8 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ScrollDownHint = ({ hintClass = ''})=>{
-    return (<div className={`inline-flex flex-col py-5 pb-7 ${hintClass}`}>
-    <span className="text-base md:text-lg">SCROLL DOWN</span>
+    return (<div className={`inline-flex flex-col pt-1 pb-4 ${hintClass}`}>
+    <span className="text-base md:text-lg whitespace-nowrap">SCROLL DOWN</span>
     <span className="inline-block w-[30px] h-[30px] mx-auto text-3xl nd:text-5xl float"><FontAwesomeIcon icon={faChevronDown}/></span>
 </div>);
 }
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -4,18 +4,22 @@ import TextCircle from './TextCircle';
 
 const Top = () =>{
     return (<section className='w-screen overflow-x-hidden overflow-y-auto'>
-        <div className="absolute text-center top-[20vh] md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2">
+        <div className="w-full absolute text-center top-[17vh] md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2">
             <p className="text-2xl md:text-4xl">{"Hello! I'm"}</p>
             <h1 className="playful font-lobster text-8xl text-white md:text-9xl">Yumi Chen</h1>
             <h2 className="w-[70vw] md:w-[80vw] lg:w-[70vw] mx-auto  flag text-center my-5 text-3xl whitespace-nowrap font-amaticSC md:text-5xl">Front-end Engineer</h2>
             <div className={`py-2 text-3xl table mx-auto h-14 md:h-20 md:text-5xl`}>
                 <SocialMedias/>
             </div>
+            <div className='block md:hidden pt-2 px-5 text-sm'>
+                <ScrollDownHint hintClass='pb-1'/>
+                <p>Latest versions of Chrome, Edge, Safari, Firefox, Opera are recommended</p>
+            </div>
             <div className='absolute top-[100px] md:top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10'>
                 <TextCircle texts="FRONT-END ENGINEER◆FRONT-END ENGINEER◆FRONT-END ENGINEER◆"/>
             </div>
         </div>
-        <div className='w-full text-center absolute bottom-10 md:bottom-7 px-5 text-sm md:text-lg'>
+        <div className='hidden md:block w-full text-center pb-10 absolute top-[100vh] transform -translate-y-full px-5 text-lg'>
             <ScrollDownHint hintClass='pb-1'/>
             <p>Latest versions of Chrome, Edge, Safari, Firefox, Opera are recommended</p>
         </div>
