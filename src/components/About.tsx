2aba89358c15083e65bd59b1102ad2373eb03ec2
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2023-12-28 15:47:44 -0400

    Add about texts

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import gsap from "gsap";
+import { useEffect } from "react";
 
 const About = () =>{    
     useEffect(()=>{
@@ -26,12 +26,12 @@ const About = () =>{
 
     return (<section className='w-[100%] flex-col space-y-5 px-7 md:px-40 pt-20 md:pt-36 pb-10 text-2xl md:text-3xl lg:leading-[3rem]'>
         <p className="about-item">
-            {"Good day! I'm a Software Engineer with 5-year experiences."}
-            {" Creating amazing and performant websites with delicate "}
+            {"Good day! I'm a Software Engineer with 5 years of success creating engaging web applications for millions of daily active users. "}
+            {"Creating amazing and performant websites with delicate "}
             {"visual effects is my passion."}
         </p>
         <p className="about-item">
-            {"Worked in scrum teams with designers, business analysts, planners, back-end developers, DBA .etc, "}
+            {"Worked in scrum teams with designers, business analysts, planners, back-end developers, DBA .etc. "}
             {"Being a mild team player, I believe that collaboration brings success and take the team and products further to greater achievement."}
         </p>
         </section>);
